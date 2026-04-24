bb58f9f635e0a4193d3ed3fcbd61c07bdc1d5de5
Subhan-Anwer <subhananwersheikh@gmail.com>
2026-04-24 12:56:47 +0500

    feat: Adding Correct Order for CMS

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,9 +1,16 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { personalDetailsType } from './personalDetailsType'
-import { codeStatsType } from './codeStatsType'
-import { techStackType } from './techStackType'
-import { servicesType } from './servicesType'
+import { type SchemaTypeDefinition } from "sanity";
+import { personalDetailsType } from "./personalDetailsType";
+import { codeStatsType } from "./codeStatsType";
+import { techStackType } from "./techStackType";
+import { servicesType } from "./servicesType";
+import { featuredProjectsType } from "./featuredProjectsType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [personalDetailsType, techStackType, codeStatsType, servicesType],
+  types: [
+    personalDetailsType,
+    techStackType,
+    codeStatsType,
+    servicesType,
+    featuredProjectsType,
+  ],
 };
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,21 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Portfolio Content')
-    .items(S.documentTypeListItems())
+    .title("Portfolio Content")
+    .items([
+      S.listItem()
+        .title("Personal Details")
+        .child(S.documentTypeList("personalDetails")),
+      S.listItem().title("Tech Stack").child(S.documentTypeList("techStack")),
+      S.listItem()
+        .title("Featured Projects")
+        .child(
+          S.documentTypeList("featuredProjects")
+            .title("Featured Projects")
+            .defaultOrdering([{ field: "serialOrder", direction: "asc" }]),
+        ),
+      S.listItem().title("Code Stats").child(S.documentTypeList("codeStats")),
+      S.listItem().title("Services").child(S.documentTypeList("services")),
+    ]);
