5e0b2d551aa164c2adbd17de956a63fd373bf8c3
Subhan-Anwer <subhananwersheikh@gmail.com>
2026-04-25 12:44:38 +0500

    feat: Added Logo Animation Type

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -4,13 +4,15 @@ import { codeStatsType } from "./codeStatsType";
 import { techStackType } from "./techStackType";
 import { servicesType } from "./servicesType";
 import { featuredProjectsType } from "./featuredProjectsType";
+import { logoAnimationType } from "./logoAnimationType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     personalDetailsType,
     techStackType,
+    featuredProjectsType,
     codeStatsType,
     servicesType,
-    featuredProjectsType,
+    logoAnimationType,
   ],
 };
