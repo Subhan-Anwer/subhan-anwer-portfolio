0709c3b392e87e44e24cfa805d9c0138eaffb60a
Subhan-Anwer <subhananwersheikh@gmail.com>
2026-04-25 17:25:40 +0500

    Update CodeStats.tsx

--- src/components/CodeStats.tsx
+++ src/components/CodeStats.tsx
@@ -1,5 +1,7 @@
 import { getCodeStats } from '@/sanity/lib/getCodeStats';
-import CodeStatsSection from './CodeStatsSection';
+import dynamic from 'next/dynamic';
+
+const CodeStatsSection = dynamic(() => import('./CodeStatsSection'));
 
 const CodeStats = async () => {
   const codeStats = await getCodeStats();
