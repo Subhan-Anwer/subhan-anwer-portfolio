3ed0bc8583d209c2bf9295d5954b0373ea319cce
Subhan-Anwer <subhananwersheikh@gmail.com>
2026-06-22 16:54:44 +0500

    feat: dynamic year in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="border-t border-white/5 py-8 text-center text-sm text-gray-300 font-light tracking-wide">
             <div className="max-w-[1200px] mx-auto px-4 group cursor-pointer">
                 <p className="">
-                    &copy; 2026{' '}
+                    &copy; {new Date().getFullYear()}{' '}
                     <span className="relative inline-block font-medium">
                         Subhan Anwer
                         <span className="absolute bottom-0 left-0 w-full h-[1px] bg-gray-300 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out origin-center" />
